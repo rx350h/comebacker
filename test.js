7782143753a563e32712b38dac9b9e3bc98a41f4
rx350h <rx350h@gmail.com>
2021-05-17 13:09:28 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 	window.addEventListener("load", function(){
-		$('body').append('<div id="comeb" style="display:none; position:fixed; top:0; left:0; width:100%; height:100%;align-items: center;justify-content: space-evenly;z-index:99999"><div style="position:relative; max-width: 450px"><a href="https://seduce.work/how-to-seduce-a-man" target="_blank"><img src="https://bigpicture.ru/wp-content/uploads/2012/07/cats-32.jpg" style="width:100%;opacity:1"></a><div class="closeb" style="position:absolute; right:10px;top:10px; font-size: 1.2em; color: #fff; cursor: pointer">X</div></div></div>');
+		$('body').append('<div id="comeb" style="display:none; position:fixed; top:0; left:0; width:100%; height:100%;align-items: center;justify-content: space-evenly;z-index:99999"><div style="position:relative; max-width: 450px"><a href="https://dolcevita-mz.com/mayim-bialik-opens-up-about-eating-disorder" target="_blank"><img src="https://bigpicture.ru/wp-content/uploads/2012/07/cats-32.jpg" style="width:100%;opacity:1"></a><div class="closeb" style="position:absolute; right:10px;top:10px; font-size: 1.2em; color: #fff; cursor: pointer">X</div></div></div>');
 
 		
 
